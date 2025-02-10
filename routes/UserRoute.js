19844baef7f94efe1f1ebf27e831e986d8f5ec15
Rutvik Bansode <159120320+rutvik2026@users.noreply.github.com>
2025-02-10 23:15:09 +0530

    Update UserRoute.js

--- routes/UserRoute.js
+++ routes/UserRoute.js
@@ -15,6 +15,8 @@ const {
   OrderCreate,
   GetOrder,
   paymentCompleteContoller,
+  feedbackController,
+  getFeedbackController,
 } = require("../controllers/UserCtrl");
 const { upload } = require("../middlewers/multer"); 
 
@@ -39,4 +41,6 @@ router.patch("/updatestatusappointment", updatestatusappointmentController);
 router.post("/orders",OrderCreate);
 router.get("/getorder",GetOrder);
 router.post("/update-payment-status",paymentCompleteContoller);
+router.post("/feed",feedbackController);
+router.get("/getfeedback", getFeedbackController);
 module.exports = router;
